d97399bee8abd7fd8db6434a816c34c0fbf1d085
wagnergbriel <wagnergabriel220596@gmail.com>
2023-09-27 19:36:23 -0300

    feat: create class portal transparencia api

--- etl/extract_data.py
+++ etl/extract_data.py
@@ -1,5 +1,9 @@
+#from airflow
+from urllib.parse import urljoin
+from os.path import join
 import config
 import requests
+import json
 
 class SancoesAPI:
     def __init__(self):
@@ -13,9 +17,50 @@ class SancoesAPI:
         else:
             print(f"Falha na solicitação. Código de status: {response.status_code}")
             return None
+    
+    def save_json_file(self, dataset, file_name):
+        with open(file_name, 'w', encoding='utf-8') as file_json:
+            json.dumps(dataset, file_json, ensure_ascii=False, indent=4)
+        print("Create file Json in {path_full}")
+
+class PortalTransparenciaAPI():
+    def __init__(self, type_database):
+        self.type_database = type_database
+        self.url = f"https://api.portaldatransparencia.gov.br/api-de-dados/{self.type_database}"
+
+    def get_all_data(self):
+        number_page = 1
+        datas_full = []
+        
+        while True:
+            url_full_all_data = urljoin(self.url, f"?pagina={number_page}")
+            print(f"Url will be connect: {url_full_all_data}")
+            response = requests.get(url_full_all_data, headers={"chave-api-dados":config.KEY_API})
+            if response.status_code == 200:
+                datas = response.json()
+                if len(datas) == 0:
+                    break
+                else:
+                    datas_full.append(datas[0])
+                    number_page += 1
+            else:
+                print(f"Falha na solicitação. Código de status: {response.status_code}")
+                break
+        return datas_full
+
+    def get_data_by_id(self, id):
+        pass
+
+    def save_json_file(self, dataset, file_name):
+        with open(file_name, 'w', encoding='utf-8') as file_json:
+            json.dumps(dataset, file_json, ensure_ascii=False, indent=4)
+        print("Create file Json in {path_full}")
 
 # Exemplo de uso:
-api = SancoesAPI()
+'''api = SancoesAPI()
 sancoes = api.get_sancoes()
 if sancoes:
-    print(sancoes)
+    print(sancoes)'''
+
+al = PortalTransparenciaAPI(type_database="acordos-leniencia").get_all_data()
+print(al)
