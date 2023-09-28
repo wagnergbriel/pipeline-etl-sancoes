a28b3c1fa16cbf31c20e4ae424656cfa10eee265
wagnergbriel <wagnergabriel220596@gmail.com>
2023-09-28 20:47:48 -0300

    feat: complete codification of class

--- etl/extract_data.py
+++ etl/extract_data.py
@@ -1,14 +1,15 @@
-#from airflow
 from urllib.parse import urljoin
 from os.path import join
+from datetime import datetime
 import config
 import requests
 import json
 
 class SancoesAPI:
-    def __init__(self):
-        self.base_url = 'https://portaldatransparencia.gov.br/sancoes/consulta/resultado?paginacaoSimples=true&tamanhoPagina=100000&offset=0&direcaoOrdenacao=asc&colunaOrdenacao=nomeSancionado&cadastro=2&colunasSelecionadas=linkDetalhamento%2Ccadastro%2CcpfCnpj%2CnomeSancionado%2CufSancionado%2Corgao%2CcategoriaSancao%2CdataPublicacao%2CvalorMulta%2Cquantidade&_=1695667939978'
+    def __init__(self, id_sancao):
+        self.base_url = f'https://portaldatransparencia.gov.br/sancoes/consulta/resultado?paginacaoSimples=true&tamanhoPagina=100000&offset=0&direcaoOrdenacao=asc&colunaOrdenacao=nomeSancionado&cadastro={id_sancao}&colunasSelecionadas=linkDetalhamento%2Ccadastro%2CcpfCnpj%2CnomeSancionado%2CufSancionado%2Corgao%2CcategoriaSancao%2CdataPublicacao%2CvalorMulta%2Cquantidade&_=1695667939978'
         self.headers = config.HEADERS
+        self.datetime_now = datetime.now().strftime("%m%Y")
     
     def get_sancoes(self):
         response = requests.get(self.base_url, headers=self.headers)
@@ -18,15 +19,18 @@ class SancoesAPI:
             print(f"Falha na solicitação. Código de status: {response.status_code}")
             return None
     
-    def save_json_file(self, dataset, file_name):
-        with open(file_name, 'w', encoding='utf-8') as file_json:
-            json.dumps(dataset, file_json, ensure_ascii=False, indent=4)
-        print("Create file Json in {path_full}")
+    def save_json_file(self, dataset, path_dir, file_name):
+        self.file_name = f"{file_name}_{self.datetime_now}.json"
+        path_full = join(path_dir, self.file_name)
+        with open(path_full, 'w', encoding='utf-8') as file_json:
+            json.dump(dataset, file_json, ensure_ascii=False, indent=4)
+        print(f"Create file Json in {path_full}")
 
 class PortalTransparenciaAPI():
     def __init__(self, type_database):
         self.type_database = type_database
         self.url = f"https://api.portaldatransparencia.gov.br/api-de-dados/{self.type_database}"
+        self.datetime_now = datetime.now().strftime("%m%Y")
 
     def get_all_data(self):
         number_page = 1
@@ -48,19 +52,48 @@ class PortalTransparenciaAPI():
                 break
         return datas_full
 
-    def get_data_by_id(self, id):
-        pass
+    def save_json_file(self, dataset, path_dir, file_name):
+        self.file_name = f"{file_name}_{self.datetime_now}.json"
+        path_full = join(path_dir, self.file_name)
+        with open(path_full, 'w', encoding='utf-8') as file_json:
+            json.dump(dataset, file_json, ensure_ascii=False, indent=4)
+        print(f"Create file Json in {path_full}")
 
-    def save_json_file(self, dataset, file_name):
-        with open(file_name, 'w', encoding='utf-8') as file_json:
-            json.dumps(dataset, file_json, ensure_ascii=False, indent=4)
-        print("Create file Json in {path_full}")
+"""
+Numbers reference type sactions
+CEIS - 1
+CNEP - 2
+CEPIM - 5
+"""
+path_datas_ceis = './data_raw/ceis'
+path_datas_cepim = './data_raw/cepim'
+path_datas_cnep = './data_raw/cnep'
+path_datas_acordo_leniencia = './data_raw/acordo_leniencia'
 
-# Exemplo de uso:
-'''api = SancoesAPI()
-sancoes = api.get_sancoes()
-if sancoes:
-    print(sancoes)'''
+api_ceis = SancoesAPI(id_sancao=1)
+data_ceis = api_ceis.get_sancoes()
+api_ceis.save_json_file(dataset=data_ceis,
+                            path_dir=path_datas_ceis,
+                            file_name="ceis"
+                            )
 
-al = PortalTransparenciaAPI(type_database="acordos-leniencia").get_all_data()
-print(al)
+api_cnep = SancoesAPI(id_sancao=2)
+data_cnep = api_cnep.get_sancoes()
+api_cnep.save_json_file(dataset=data_cnep,
+                            path_dir=path_datas_cnep,
+                            file_name="cnep"
+                            )
+
+api_cepim = SancoesAPI(id_sancao=5)
+data_cepim = api_cepim.get_sancoes()
+api_cepim.save_json_file(dataset=data_cepim,
+                            path_dir=path_datas_cepim,
+                            file_name="cepim"
+                            )
+
+api_portal = PortalTransparenciaAPI(type_database="acordos-leniencia")
+datas = api_portal.get_all_data()
+api_portal.save_json_file(dataset=datas,
+                            path_dir=path_datas_acordo_leniencia,
+                            file_name="acordo_leniencia"
+                            )
